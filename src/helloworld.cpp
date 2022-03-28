fd3e57f837471f1b6a361d5af62f698a5e33d816
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2022-03-27 22:32:19 -0600

    feat: scaffold basic Window

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,18 +1,86 @@
 #include <windows.h>
 
-int CALLBACK WinMain(
-  HINSTANCE hInstance,
-  HINSTANCE hPrevInstance,
-  PSTR lpCmdLine,
-  INT nCmdShow
+WNDPROC Wndproc;
+
+LRESULT CALLBACK MainWindowCallback(
+  HWND Window,
+  UINT Message,
+  WPARAM WParam,
+  LPARAM LParam
 )
 {
-  MessageBoxA(
-    0,
-    "This is Handmade Hero.",
-    "Handmade Hero",
-    MB_OK | MB_ICONINFORMATION
-  );
+  LRESULT Result = 0;
+
+  switch (Message)
+  {
+  case WM_SIZE:
+    break;
+  case WM_DESTROY:
+    break;
+  case WM_CLOSE:
+    break;
+  case WM_ACTIVATE:
+    break;
+  default:
+    Result = DefWindowProc(Window, Message, WParam, LParam);
+    break;
+  }
+
+  return Result;
+}
+
+int CALLBACK WinMain(
+  HINSTANCE Instance,
+  HINSTANCE PrevInstance,
+  PSTR CommandLine,
+  INT ShowCode
+)
+{
+  WNDCLASSA WindowClass = {};
+
+  WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+  WindowClass.lpfnWndProc = MainWindowCallback;
+  WindowClass.hInstance = Instance;
+  // WindowClass.hIcon = ;
+  WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+  if (RegisterClassA(&WindowClass)) {
+    HWND WindowHandle = CreateWindowExA(
+      0,
+      WindowClass.lpszClassName,
+      "Handmade Hero",
+      WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      0,
+      0,
+      Instance,
+      0
+    );
+
+    if (WindowHandle) {
+      MSG Message;
+
+      for (;;) {
+        BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+        if (MessageResult > 0) {
+          TranslateMessage(&Message);
+          DispatchMessage(&Message);
+        }
+        else {
+          break;
+        }
+      }
+    }
+    else {
+      // TODO logging
+    }
+  }
+  else {
+    // TODO logging
+  }
 
   return 0;
 }
